e01f86b0a1d95877a5772b1fe240319f8bdddf1d
Johann Gerell <jgel@tobii.com>
2020-07-05 22:23:49 +0200

    Added gridlines

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,69 @@ public:
         , m_fill{std::move(fill)}
     {}
 
+    svg_builder& with_grid(size_t x, size_t y, std::string stroke)
+    {
+        m_grid = true;
+        m_grid_x = x;
+        m_grid_y = y;
+        m_grid_stroke = stroke;
+        return *this;
+    }
+    
     std::string build()
     {
-        auto attribute = [] (const char* name, const std::string& value)
+        auto attribute_i = [] (const char* name, size_t value)
+        {
+            return std::string(name).append("=\"").append(std::to_string(value)).append("\"");
+        };
+
+        auto attribute_s = [] (const char* name, std::string_view value)
         {
             return std::string(name).append("=\"").append(value).append("\"");
         };
 
         std::string svg;
-        svg += "<svg " + attribute("width", std::to_string(m_width)) + " " + attribute("height", std::to_string(m_height)) + " version=\"1.1\" baseProfile=\"full\" xmlns=\"http://www.w3.org/2000/svg\">\n";
-        svg += "  <rect width=\"100%\" height=\"100%\" " + attribute("fill", m_fill) + " />\n";
+        svg += "<svg " +
+               attribute_i("width", m_width) + " " +
+               attribute_i("height", m_height) + " " +
+               attribute_s("version", "1.1") + " " +
+               attribute_s("baseProfile", "full") + " " +
+               attribute_s("xmlns", "http://www.w3.org/2000/svg") +
+               ">\n";
+
+        svg += "<rect " +
+               attribute_s("width", "100%") + " " +
+               attribute_s("height", "100%") + " " +
+               attribute_s("fill", m_fill) +
+               " />\n";
+
+        if (m_grid)
+        {
+            for (size_t x = m_grid_x; x <= m_width; x += m_grid_x)
+            {
+                svg += "<line " +
+                       attribute_i("x1", x) + " " +
+                       attribute_i("x2", x) + " " +
+                       attribute_i("y1", 0) + " " +
+                       attribute_i("y2", m_height) + " " +
+                       attribute_s("stroke", m_grid_stroke) + " " +
+                       attribute_i("stroke-width", 1) +
+                       " />\n";
+            }
+
+            for (size_t y = m_grid_y; y <= m_height; y += m_grid_y)
+            {
+                svg += "<line " +
+                       attribute_i("x1", 0) + " " +
+                       attribute_i("x2", m_width) + " " +
+                       attribute_i("y1", y) + " " +
+                       attribute_i("y2", y) + " " +
+                       attribute_s("stroke", m_grid_stroke) + " " +
+                       attribute_i("stroke-width", 1) +
+                       " />\n";
+            }
+        }
+
         svg += "</svg>\n";
 
         return svg;
@@ -31,11 +84,16 @@ private:
     size_t m_width{};
     size_t m_height{};
     std::string m_fill{"white"};
+    bool m_grid{};
+    size_t m_grid_x{};
+    size_t m_grid_y{};
+    std::string m_grid_stroke{"lightgrey"};
 };
 
 int main()
 {
-    svg_builder svg{400, 250, "grey"};
+    svg_builder svg{400, 250, "white"};
+    svg.with_grid(50, 50, "#eeeeee");
 
     std::cout << svg.build() << std::endl;
 }
