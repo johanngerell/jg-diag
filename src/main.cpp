f5ba4fced37516ce1761f4eaeafb2cdd916e5d22
Johann Gerell <jgel@tobii.com>
2020-07-06 14:49:32 +0200

    Changes...

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,74 @@
 class xml_tag final
 {
 public:
+    static xml_tag root(std::string& xml, std::string name)
+    {
+        return {xml, name};
+    }
+
+    static xml_tag child(xml_tag& parent, std::string name)
+    {
+        return {parent, name};
+    }
+
+    xml_tag(xml_tag&& other)
+        : m_xml{other.m_xml}
+        , m_name{std::move(other.m_name)}
+        , m_is_parent{other.m_is_parent}
+    {
+        other.m_xml = nullptr;
+        other.m_is_parent = false;
+    }
+
+    ~xml_tag()
+    {
+        if (m_xml)
+        {
+            if (m_is_parent)
+                m_xml->append("</").append(m_name).append(">");
+            else
+                m_xml->append(" />");
+            
+            if (!m_is_inline)
+                m_xml->append("\n");
+        }
+    }
+
+    xml_tag& attribute(std::string_view name, size_t value)
+    {
+        m_xml->append(1, ' ').append(name).append("=\"").append(std::to_string(value)).append("\"");
+        return *this;
+    }
+
+    xml_tag& attribute(std::string_view name, std::string_view value)
+    {
+        m_xml->append(1, ' ').append(name).append("=\"").append(value).append("\"");
+        return *this;
+    }
+
+    xml_tag& text(std::string_view text)
+    {
+        if (!m_is_parent)
+        {
+            m_is_parent = true;
+            m_xml->append(">");
+        }
+
+        m_xml->append(text);
+        return *this;
+    }
+
+    void no_newline()
+    {
+        m_is_inline = true;
+    }
+
+private:
     xml_tag(std::string& xml, std::string name)
-        : m_xml{xml}
+        : m_xml{&xml}
         , m_name{std::move(name)}
     {
-        m_xml.append("<").append(m_name);
+        m_xml->append("<").append(m_name);
     }
 
     xml_tag(xml_tag& parent, std::string name)
@@ -18,45 +81,18 @@ public:
         if (!parent.m_is_parent)
         {
             parent.m_is_parent = true;
-            m_xml.append(">\n");
+
+            if (!parent.m_is_inline)
+                m_xml->append(">\n");
         }
 
-        m_xml.append("<").append(m_name);
+        m_xml->append("<").append(m_name);
     }
 
-    ~xml_tag()
-    {
-        if (m_is_parent)
-            m_xml.append("</").append(m_name).append(">\n");
-        else
-            m_xml.append(" />\n");
-    }
-
-    void attribute(std::string_view name, size_t value)
-    {
-        m_xml.append(1, ' ').append(name).append("=\"").append(std::to_string(value)).append("\"");
-    }
-
-    void attribute(std::string_view name, std::string_view value)
-    {
-        m_xml.append(1, ' ').append(name).append("=\"").append(value).append("\"");
-    }
-
-    void text(std::string_view text)
-    {
-        if (!m_is_parent)
-        {
-            m_is_parent = true;
-            m_xml.append(">");
-        }
-
-        m_xml.append(text);
-    }
-
-private:
-    std::string& m_xml;
+    std::string* m_xml;
     std::string m_name;
     bool m_is_parent{};
+    bool m_is_inline{};
 };
 
 class svg_builder final
@@ -97,7 +133,7 @@ public:
         std::string xml;
         
         {
-            xml_tag svg{xml, "svg"};
+            auto svg = xml_tag::root(xml, "svg");
             svg.attribute("width", m_width);
             svg.attribute("height", m_height);
             svg.attribute("version", "1.1");
@@ -105,74 +141,69 @@ public:
             svg.attribute("xmlns", "http://www.w3.org/2000/svg");
 
             if (m_background)
-            {
-                xml_tag background{svg, "rect"};
-                background.attribute("width", "100%");
-                background.attribute("height", "100%");
-                background.attribute("fill", m_background_fill);
-            }
+                xml_tag::child(svg, "rect")
+                    .attribute("width", "100%")
+                    .attribute("height", "100%")
+                    .attribute("fill", m_background_fill);
 
             if (m_grid)
             {
                 for (size_t x = m_grid_x; x <= m_width; x += m_grid_x)
-                {
-                    xml_tag vertical{svg, "line"};
-                    vertical.attribute("x1", x);
-                    vertical.attribute("x2", x);
-                    vertical.attribute("y1", 0);
-                    vertical.attribute("y2", m_height);
-                    vertical.attribute("stroke", m_grid_stroke);
-                    vertical.attribute("stroke-width", 1);
-                }
+                    xml_tag::child(svg, "line")
+                        .attribute("x1", x)
+                        .attribute("x2", x)
+                        .attribute("y1", 0)
+                        .attribute("y2", m_height)
+                        .attribute("stroke", m_grid_stroke)
+                        .attribute("stroke-width", 1);
 
                 for (size_t y = m_grid_y; y <= m_height; y += m_grid_y)
-                {
-                    xml_tag horizontal{svg, "line"};
-                    horizontal.attribute("x1", 0);
-                    horizontal.attribute("x2", m_width);
-                    horizontal.attribute("y1", y);
-                    horizontal.attribute("y2", y);
-                    horizontal.attribute("stroke", m_grid_stroke);
-                    horizontal.attribute("stroke-width", 1);
-                }
+                    xml_tag::child(svg, "line")
+                        .attribute("x1", 0)
+                        .attribute("x2", m_width)
+                        .attribute("y1", y)
+                        .attribute("y2", y)
+                        .attribute("stroke", m_grid_stroke)
+                        .attribute("stroke-width", 1);
             }
 
-            {
-                xml_tag frame1{svg, "rect"};
-                frame1.attribute("x", 50);
-                frame1.attribute("y", 50);
-                frame1.attribute("width", 300); 
-                frame1.attribute("height", 50);
-                frame1.attribute("stroke", "black");
-                frame1.attribute("fill", "transparent");
-                frame1.attribute("stroke-width", 3);
-            }
+            xml_tag::child(svg, "rect")
+                .attribute("x", 50)
+                .attribute("y", 50)
+                .attribute("width", 300)
+                .attribute("height", 50)
+                .attribute("stroke", "black")
+                .attribute("fill", "transparent")
+                .attribute("stroke-width", 3);
 
-            {
-                xml_tag text1{svg, "text"};
-                text1.attribute("x", 75);
-                text1.attribute("y", 75);
-                text1.attribute("font-size", 25);
-                text1.attribute("font-family", "sans-serif");
-                text1.attribute("font-weight", "bold");
-                text1.attribute("text-anchor", "left");
-                text1.attribute("dominant-baseline", "middle");
-                text1.attribute("stroke", "black");
-                text1.attribute("fill", "white");
-                text1.text("This is <tspan font-weight=\"bold\" fill=\"red\">bold and red</tspan>");
-            }
+            std::string s{"This is "};
+            xml_tag::root(s, "tspan")
+                .attribute("font-weight", "bold")
+                .attribute("fill", "red")
+                .text("bold and red")
+                .no_newline();
+
+            xml_tag::child(svg, "text")
+                .attribute("x", 75)
+                .attribute("y", 75)
+                .attribute("font-size", 25)
+                .attribute("font-family", "sans-serif")
+                .attribute("font-weight", "bold")
+                .attribute("text-anchor", "left")
+                .attribute("dominant-baseline", "middle")
+                .attribute("stroke", "black")
+                .attribute("fill", "white")
+                .text(s);
 
             if (m_border)
-            {
-                xml_tag border{svg, "rect"};
-                border.attribute("x", 0);
-                border.attribute("y", 0);
-                border.attribute("width", m_width); 
-                border.attribute("height", m_height);
-                border.attribute("stroke", m_border_stroke);
-                border.attribute("fill", "transparent");
-                border.attribute("stroke-width", 1);
-            }
+                xml_tag::child(svg, "rect")
+                    .attribute("x", 0)
+                    .attribute("y", 0)
+                    .attribute("width", m_width)
+                    .attribute("height", m_height)
+                    .attribute("stroke", m_border_stroke)
+                    .attribute("fill", "transparent")
+                    .attribute("stroke-width", 1);
         }
 
         return xml;
