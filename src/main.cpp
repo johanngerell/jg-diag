f2779c84e493a0567d397fceb023cb36fde89e44
Johann Gerell <jgel@tobii.com>
2020-07-09 21:23:30 +0200

    Ditched fluent interface and renamed to xml_writer

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,38 @@
 #include <string>
 #include <string_view>
 
-class xml_tag final
+class xml_writer final
 {
 public:
-    xml_tag(std::ostream& stream, std::string name)
-        : m_stream{&stream}
-        , m_name{std::move(name)}
+    static xml_writer root_element(std::ostream& stream, std::string name)
     {
-        *m_stream << "<" << m_name;
+        return {stream, std::move(name)};
     }
 
-    xml_tag(xml_tag& parent, std::string name)
-        : m_stream{parent.m_stream}
-        , m_name{std::move(name)}
+    static xml_writer child_element(xml_writer& parent, std::string name)
     {
-        if (!parent.m_is_parent)
-        {
-            parent.m_is_parent = true;
-
-            if (!parent.m_is_inline)
-                *m_stream << ">\n";
-        }
-
-        *m_stream << "<" << m_name;
+        return {parent, std::move(name)};
     }
 
-    ~xml_tag()
+    xml_writer(xml_writer&& other)
+        : m_stream{other.m_stream}
+        , m_name{std::move(other.m_name)}
+        , m_is_parent{other.m_is_parent}
+    {
+        other.m_stream = nullptr;
+        other.m_is_parent = false;
+    }
+
+    xml_writer& operator=(xml_writer&& other)
+    {
+        m_stream = other.m_stream;
+        m_name = std::move(other.m_name);
+        m_is_parent = other.m_is_parent;
+        other.m_stream = nullptr;
+        other.m_is_parent = false;
+    }
+
+    ~xml_writer()
     {
         if (m_stream)
         {
@@ -37,24 +43,21 @@ public:
             else
                 *m_stream << " />";
             
-            if (!m_is_inline)
-                *m_stream << "\n";
+            *m_stream << "\n";
         }
     }
 
-    xml_tag& attribute(std::string_view name, size_t value)
+    void write_attribute(std::string_view name, size_t value)
     {
         *m_stream << ' ' << name << "=\"" << value << "\"";
-        return *this;
     }
 
-    xml_tag& attribute(std::string_view name, std::string_view value)
+    void write_attribute(std::string_view name, std::string_view value)
     {
         *m_stream << ' ' << name << "=\"" << value << "\"";
-        return *this;
     }
 
-    xml_tag& text(std::string_view text)
+    void write_text(std::string_view text)
     {
         if (!m_is_parent)
         {
@@ -63,19 +66,33 @@ public:
         }
 
         *m_stream << text;
-        return *this;
-    }
-
-    void no_newline()
-    {
-        m_is_inline = true;
     }
 
 private:
+    xml_writer(std::ostream& stream, std::string name)
+        : m_stream{&stream}
+        , m_name{std::move(name)}
+    {
+        *m_stream << "<" << m_name;
+    }
+
+    xml_writer(xml_writer& parent, std::string name)
+        : m_stream{parent.m_stream}
+        , m_name{std::move(name)}
+    {
+        if (!parent.m_is_parent)
+        {
+            parent.m_is_parent = true;
+
+            *m_stream << ">\n";
+        }
+
+        *m_stream << "<" << m_name;
+    }
+
     std::ostream* m_stream{};
     std::string m_name;
     bool m_is_parent{};
-    bool m_is_inline{};
 };
 
 struct svg_point final
@@ -158,105 +175,108 @@ public:
     svg_writer(std::ostream& stream, svg_size size)
         : m_stream{stream}
         , m_size{size}
-        , m_svg{m_stream, "svg"}
+        , m_root{xml_writer::root_element(m_stream, "svg")}
     {
-        m_svg
-            .attribute("width", m_size.width)
-            .attribute("height", m_size.height)
-            .attribute("version", "1.1")
-            .attribute("baseProfile", "full")
-            .attribute("xmlns", "http://www.w3.org/2000/svg");
+        m_root.write_attribute("width", m_size.width);
+        m_root.write_attribute("height", m_size.height);
+        m_root.write_attribute("version", "1.1");
+        m_root.write_attribute("baseProfile", "full");
+        m_root.write_attribute("xmlns", "http://www.w3.org/2000/svg");
     }
 
     void write_background(std::string_view color = "white")
     {
-        xml_tag{m_svg, "rect"}
-            .attribute("width", "100%")
-            .attribute("height", "100%")
-            .attribute("fill", color);
+        auto tag = xml_writer::child_element(m_root, "rect");
+        tag.write_attribute("width", "100%");
+        tag.write_attribute("height", "100%");
+        tag.write_attribute("fill", color);
     }
 
     void write_grid(size_t distance, std::string_view color = "whitesmoke")
     {
         for (size_t i = distance; i <= m_size.width; i += distance)
-            xml_tag{m_svg, "line"}
-                .attribute("x1", i)
-                .attribute("x2", i)
-                .attribute("y1", 0)
-                .attribute("y2", m_size.height)
-                .attribute("stroke", color)
-                .attribute("stroke-width", 1);
+        {
+            auto tag = xml_writer::child_element(m_root, "line");
+            tag.write_attribute("x1", i);
+            tag.write_attribute("x2", i);
+            tag.write_attribute("y1", 0);
+            tag.write_attribute("y2", m_size.height);
+            tag.write_attribute("stroke", color);
+            tag.write_attribute("stroke-width", 1);
+        }
 
         for (size_t i = distance; i <= m_size.height; i += distance)
-            xml_tag{m_svg, "line"}
-                .attribute("x1", 0)
-                .attribute("x2", m_size.width)
-                .attribute("y1", i)
-                .attribute("y2", i)
-                .attribute("stroke", color)
-                .attribute("stroke-width", 1);
+        {
+            auto tag = xml_writer::child_element(m_root, "line");
+            tag.write_attribute("x1", 0);
+            tag.write_attribute("x2", m_size.width);
+            tag.write_attribute("y1", i);
+            tag.write_attribute("y2", i);
+            tag.write_attribute("stroke", color);
+            tag.write_attribute("stroke-width", 1);
+        }
     }
 
     void write_border(std::string_view color = "black")
     {
-        xml_tag{m_svg, "rect"}
-            .attribute("x", 0)
-            .attribute("y", 0)
-            .attribute("width", m_size.width)
-            .attribute("height", m_size.height)
-            .attribute("stroke", color)
-            .attribute("fill", "transparent")
-            .attribute("stroke-width", 1);
+        auto tag = xml_writer::child_element(m_root, "rect");
+        tag.write_attribute("x", 0);
+        tag.write_attribute("y", 0);
+        tag.write_attribute("width", m_size.width);
+        tag.write_attribute("height", m_size.height);
+        tag.write_attribute("stroke", color);
+        tag.write_attribute("fill", "transparent");
+        tag.write_attribute("stroke-width", 1);
     }
 
     void write_rect(svg_rect rect, const svg_rect_attributes& attributes)
     {
-        xml_tag{m_svg, "rect"}
-            .attribute("x", rect.x)
-            .attribute("y", rect.y)
-            .attribute("width", rect.width)
-            .attribute("height", rect.height)
-            .attribute("stroke", attributes.stroke)
-            .attribute("fill", attributes.fill)
-            .attribute("stroke-width", attributes.stroke_width);
+        auto tag = xml_writer::child_element(m_root, "rect");
+        tag.write_attribute("x", rect.x);
+        tag.write_attribute("y", rect.y);
+        tag.write_attribute("width", rect.width);
+        tag.write_attribute("height", rect.height);
+        tag.write_attribute("stroke", attributes.stroke);
+        tag.write_attribute("fill", attributes.fill);
+        tag.write_attribute("stroke-width", attributes.stroke_width);
     }
 
     void write_text(svg_point point, const svg_text_attributes& attributes, std::string_view text)
     {
-        xml_tag{m_svg, "text"}
-            .attribute("x", point.x)
-            .attribute("y", point.y)
-            .attribute("font-size", attributes.font_size)
-            .attribute("font-family", attributes.font_family)
-            .attribute("font-weight", attributes.font_weight)
-            .attribute("text-anchor", to_string(attributes.text_anchor))
-            .attribute("dominant-baseline", to_string(attributes.dominant_baseline))
-            .attribute("fill", attributes.fill)
-            .attribute("stroke", attributes.stroke)
-            .text(text);
+        auto tag = xml_writer::child_element(m_root, "text");
+        tag.write_attribute("x", point.x);
+        tag.write_attribute("y", point.y);
+        tag.write_attribute("font-size", attributes.font_size);
+        tag.write_attribute("font-family", attributes.font_family);
+        tag.write_attribute("font-weight", attributes.font_weight);
+        tag.write_attribute("text-anchor", to_string(attributes.text_anchor));
+        tag.write_attribute("dominant-baseline", to_string(attributes.dominant_baseline));
+        tag.write_attribute("fill", attributes.fill);
+        tag.write_attribute("stroke", attributes.stroke);
+        tag.write_text(text);
     }
 
 private:
     std::ostream& m_stream;
     svg_size m_size;
-    xml_tag m_svg;
+    xml_writer m_root;
 };
 
 int main()
 {
+    svg_writer svg{std::cout, {1024, 768}};
+    svg.write_background();
+    svg.write_grid(50);
+
     svg_rect_attributes default_rect;
     default_rect.fill = "whitesmoke";
     default_rect.stroke = "black";
     default_rect.stroke_width = "3";
+    svg.write_rect({50, 50, 300, 50}, default_rect);
 
     svg_text_attributes default_text;
     default_text.font_size = "25";
     default_text.font_weight = "bold";
-
-    svg_writer svg{std::cout, {1024, 768}};
-    svg.write_background();
-    svg.write_grid(50);
-    svg.write_rect({50, 50, 300, 50}, default_rect);
     svg.write_text({75, 75}, default_text, "SomeName");
     svg.write_border();
 }
